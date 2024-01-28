2e50fafd5c0b8444aa7641649fd41cb28b349bc5
Psycotik <sgtpsycotik@gmail.com>
2024-01-27 17:32:10 -0800

    Created TempConvert java file. Added error messages to README file. Utilized method recursion for fun.

---
+++ src/TempConvert.java
@@ -0,0 +1,65 @@
+/**
+ * @author James Ward
+ *
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class TempConvert {
+
+    public static void main(String[] args)
+    {
+        mainMethod();
+    }
+
+    private static void mainMethod() {
+        int line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 1 for Temperature conversion tool, 2 for Time conversion tool, 3 to exit program.");
+        line = in.nextInt();
+
+        if (line == 1) {
+            tempConverter();
+            mainMethod();
+        } else if (line == 2) {
+            timeConverter();
+            mainMethod();
+        } else if (line == 3) {
+            System.out.println("Program ended.");
+        } else{
+        System.out.println("Invalid Input");
+        mainMethod();
+        }
+    }
+
+    private static void tempConverter() {
+        double line;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a temperature in Celsius: ");
+        line = in.nextDouble();
+
+        double Fahrenheit = line * 9/5 + 32;
+
+        System.out.printf("%.1f degrees C converts to %.1f degrees F\n",line, Fahrenheit);
+    }
+    
+    private static void timeConverter() {
+        int line, hours, minutes, seconds;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a total number of seconds: ");
+        line = in.nextInt();
+
+        hours = line / 3600;
+        minutes = (line%(hours*3600)) / 60;
+        seconds = (line%(hours*3600+minutes*60));
+
+        System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds.\n", line, hours, minutes, seconds);
+        
+    }
+
+}

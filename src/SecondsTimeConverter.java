f4f5c62c649b92dfed2b106ddae50c23f33f58af
Rigel <rigelcluciani@gmail.com>
2024-02-03 20:38:21 -0800

    2/3/2024 Rigel Luciani

---
+++ src/SecondsTimeConverter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+// This one just prints because I'm not that good with popup windows yet
+public class SecondsTimeConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of seconds: ");
+        int seconds = scanner.nextInt();
+        int remainingSeconds = seconds % 60;
+        int minutes = (seconds / 60) % 60;
+        int hours = seconds / 3600;
+
+        System.out.println("The number of hours passed is: " + hours);
+        System.out.println("The number of minutes passed is: " + minutes);
+        System.out.println("The number of seconds left is: " + remainingSeconds);
+    }
+}

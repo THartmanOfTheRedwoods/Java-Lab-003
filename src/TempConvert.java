58dc8ef077a526a31a9e7503b046d16cf8b402a2
jcheong641 <jcheong641@mycr.redwoods.edu>
2024-02-03 21:20:51 -0800

    Completed Lab 03

---
+++ src/TempConvert.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class TempConvert {
+    public static void main(String[] args) {
+        System.out.print("Enter a temperature in Celsius: ");
+
+        Scanner scanner = new Scanner(System.in);
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = celsius * 9/5 + 32;
+
+        System.out.printf("%.1f C = %.1f F\n", celsius, fahrenheit);
+
+        scanner.close();
+    }
+}

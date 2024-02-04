457b2c767401d314a0b9861a66884475e97f6c96
Jaime <jaimeortiz0724@gmail.com>
2024-02-03 16:53:57 -0800

    Lab-003 finished

---
+++ src/Format.java
@@ -0,0 +1,15 @@
+public class Format {
+
+    public static void main(String[] args) {
+
+        int Value = 24;
+        System.out.printf("%f", Value);
+
+        double doubleValue = 6.42;
+        System.out.printf("%d", doubleValue);
+
+        int Value1 = 42;
+        System.out.printf("%d %s", Value1);
+
+    }
+}
